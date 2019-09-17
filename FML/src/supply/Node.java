2de627113ea1b58a6be50083e15184f893688c69
davidstevenrose <dsrose3626@eagle.fgcu.edu>
2019-09-17 11:20:27 -0400

    Added traversal methods in Node class.

--- FML/src/supply/Node.java
+++ FML/src/supply/Node.java
@@ -19,24 +19,46 @@ public class Node<T> {
     public Node<T> getLeft() {
         return left;
     }
-
     public void setLeft(Node<T> left) {
         this.left = left;
     }
-
     public Node<T> getRight() {
         return right;
     }
-
     public void setRight(Node<T> right) {
         this.right = right;
     }
-
     public T getData() {
         return data;
     }
-
     public void setData(T data) {
         this.data = data;
+    }    
+    public void printPreOrder(){
+        if(this==null){
+            return;
+        }else{
+            System.out.println(data.toString());
+            left.printPreOrder();
+            right.printPostOrder();
+        }
+    }
+    public void printPostOrder(){
+        if(this==null){
+            return;
+        }else{            
+            left.printPreOrder();
+            right.printPostOrder();
+            System.out.println(data.toString());
+        }
+    }
+    public void printInOrder(){
+        if(this==null){
+            return;
+        }else{            
+            left.printPreOrder();
+            System.out.println(data.toString());
+            right.printPostOrder();
+        }
     }
 }
